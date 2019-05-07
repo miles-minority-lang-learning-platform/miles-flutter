1acf7e8ac9d797b5c2f7ce129db82620ea3177cd
weekbin <18717744742@163.com>
2019-05-07 09:48:09 +0800

    📌 重写用户点击返回事件

--- lib/tomato-agency/period.dart
+++ lib/tomato-agency/period.dart
@@ -109,50 +109,59 @@ class PeroidState extends State<Peroid> with WidgetsBindingObserver{
 
   @override
   build(BuildContext context) => Scaffold(
-        body: Container(
-          color: _bgColor,
-          child: Stack(
-            children: <Widget>[
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      _currentTime.toString().substring(2, 7),
-                      style: TextStyle(color: Colors.white, fontSize: 60),
-                    ),
-                    Text(
-                      _text,
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 50,
-                  width: 120,
-                  margin: EdgeInsets.only(bottom: 50),
-                  child: RaisedButton(
-                    onPressed: () => {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (BuildContext context) =>
-                                      BeforeResult("interrupted")))
-                        },
-                    color: Colors.red[200],
-                    textColor: Colors.white,
-                    child: Text(
-                      "放弃",
-                      style: TextStyle(fontSize: 20),
-                    ),
+    // 加入willpopscope，重写用户点击返回键事件
+    // 用户点击返回键之后，将用户导入“是否要放弃”的界面，而不是直接返回
+        body: WillPopScope(
+          onWillPop: (){
+            Navigator.push(context, MaterialPageRoute(
+              builder: (BuildContext context) => BeforeResult("interrupted")
+            ));
+          },
+          child: Container(
+            color: _bgColor,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        _currentTime.toString().substring(2, 7),
+                        style: TextStyle(color: Colors.white, fontSize: 60),
+                      ),
+                      Text(
+                        _text,
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 50,
+                    width: 120,
+                    margin: EdgeInsets.only(bottom: 50),
+                    child: RaisedButton(
+                      onPressed: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        BeforeResult("interrupted")))
+                          },
+                      color: Colors.red[200],
+                      textColor: Colors.white,
+                      child: Text(
+                        "放弃",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       );
